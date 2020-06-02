fc870adbe374ea084fee775d05c81c0b1b0421ee
Daniel Tian <dtian.95@gmail.com>
2020-06-02 18:16:46 +0800

    created hello world api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Zigma" });
 });
 
+router.get("/hello", function (req, res) {
+  res.json({ Hello: "World!" });
+});
+
 module.exports = router;
